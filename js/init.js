5267ae027013972e657e566f1806fd7f0b6a031c
Raymart Baeza <incomeandexpense@gmail.com>
2017-09-13 16:05:31 +0800

    Push incomplete capstone1 project

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $(".button-collapse").sideNav();
+    $('.slider').slider({full_width: true});	
+}); 
+
+  // end of document ready
+})(jQuery); // end of jQuery name space
+
